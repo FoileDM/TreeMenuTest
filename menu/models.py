573a2dfc0664829b23ffcf2ca8101e952978e45d
Vlad <foile.dm@gmail.com>
2025-07-27 15:21:19 +0300

    feat(models): add hierarchical Menu model with admin integration
    - Introduced `Menu` model supporting parent-child relationships.
    - Implemented path calculation for hierarchical structure.
    - Registered the model in admin with list display, search, and filters.
    - Create migrations.

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+    path = models.CharField(max_length=500, editable=False)
+
+    def save(self, *args, **kwargs):
+        old_path = None
+        if self.pk:
+            try:
+                old_path = Menu.objects.get(pk=self.pk).path
+            except Menu.DoesNotExist:
+                old_path = None
+
+        if self.parent:
+            self.path = f"{self.parent.path}/{self.name}"
+        else:
+            self.path = self.name
+
+        super().save(*args, **kwargs)
+
+        if old_path and self.path != old_path:
+            prefix = old_path + '/'
+            descendants = Menu.objects.filter(path__startswith=prefix)
+            for desc in descendants:
+                desc.path = self.path + desc.path[len(old_path):]
+                desc.save(update_fields=['path'])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Menu item"
+        verbose_name_plural = "Menu items"
+        ordering = ['id']
