bf22ca557ac54745b0fa0f4738c94826a3f836dd
Vlad <foile.dm@gmail.com>
2025-07-28 09:27:58 +0300

    feat(views): update `MenuView` to filter items dynamically based on path
    - Introduced dynamic filtering of `Menu` items based on the active path.
    - Improved handling of root menus and hierarchical structure.

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.template.response import TemplateResponse
 from django.views import View
@@ -8,11 +9,20 @@ from menu.models import Menu
 class MenuView(View):
 
     def get(self, request: HttpRequest, path: str | None = None) -> HttpResponse:
-        items = Menu.objects.all()
+
+        active_path = path or ""
+        if active_path:
+            root_segment = active_path.split("/", 1)[0]
+            filters = Q(parent__isnull=True) | Q(path__startswith=root_segment)
+        else:
+            filters = Q(parent__isnull=True) | Q(parent__parent__isnull=True)
+
+        qs = Menu.objects.filter(filters)
+        items = [item for item in qs]
         root_menus = [item for item in items if item.parent_id is None]
 
         context = {
-            "active_path": path or "",
+            "active_path": active_path,
             "root_menus": root_menus,
             "menu_items": items,
         }
